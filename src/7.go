8a14b2969093b8be6668e903fbde27556625bc55
nemorioreds <nemorioreds@gmail.com>
2025-03-13 10:28:01 +0000

    Add 7.go

---
+++ src/7.go
@@ -0,0 +1,17 @@
+
+func main() {
+
+    fmt.Println("Enter two numbers to find their sum")
+
+     var num1 int
+
+     var num2 int
+
+      num1 = 5
+
+      num2 = 3
+
+       sum := num1 + num2
+
+        fmt.Println(sum)
+}
