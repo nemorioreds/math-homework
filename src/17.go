103241c8c0d3860f82615b32ca36697fddfd7f83
nemorioreds <nemorioreds@gmail.com>
2025-03-23 21:15:00 +0000

    Add 17.go

---
+++ src/17.go
@@ -0,0 +1,9 @@
+def find_smallest_negative_number(numbers):
+    """
+    This function takes a list of numbers and returns the smallest negative number.
+    
+    Example usage:
+    >>> find_smallest_negative_number([10, -2, 3, 5])
+    -2
+    """
+    return min(numbers)
