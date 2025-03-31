ffb722b530d9f15241eb0b8d688471b9cc70cfb7
nemorioreds <nemorioreds@gmail.com>
2025-03-31 06:27:24 +0000

    Add 21.go

---
+++ src/21.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    // Example of a function that performs basic arithmetic operations
+    result := 2 + 3 * 4 - 5 / 2
+    fmt.Printf("Result: %d\n", result)
+}
