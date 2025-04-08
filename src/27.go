35839dbe2d1c842612be6010e0ff324981336bd7
nemorioreds <nemorioreds@gmail.com>
2025-04-08 16:21:02 +0000

    Add 27.go

---
+++ src/27.go
@@ -0,0 +1,15 @@
+def fib_sequence(n):
+    if n <= 0:
+        return "Please enter a positive integer."
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    sequence = [0, 1]
+    for i in range(2, n):
+        next_value = sequence[-1] + sequence[-2]
+        sequence.append(next_value)
+    return sequence
+
+print(fib_sequence(10))
