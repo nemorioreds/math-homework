1e9e45728b1f5f324a847a9693e3baa96e5fc45d
nemorioreds <nemorioreds@gmail.com>
2025-05-23 07:07:08 +0000

    Add 38.go

---
+++ src/38.go
@@ -0,0 +1,11 @@
+def multiply(a: int, b: int) -> int:
+    """Multiply two integers."""
+    return a * b
+
+def add(a: int, b: int) -> int:
+    """Add two integers."""
+    return a + b
+
+# Example usage
+print("Multiplying 5 and 3:", multiply(5, 3))
+print("Adding 4 and 6:", add(4, 6))
