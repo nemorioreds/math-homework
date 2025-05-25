c0fb9a5d34d7d1e6f6e2add20ffdb846f4ef6b1a
nemorioreds <nemorioreds@gmail.com>
2025-05-25 06:30:41 +0000

    Add 40.go

---
+++ src/40.go
@@ -0,0 +1,14 @@
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i == 0:
+            factors.append(i)
+            n //= i
+        else:
+            i += 1
+    if n > 1:
+        factors.append(n)
+    return factors
+
+print(prime_factors(36)) # Output: [2, 2, 3, 3]
